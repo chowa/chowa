a8a7035ccad41e6bd0e4bbb23faffc3f47d611bd
smoking rabbit <xuecong8226@gmail.com>
2020-01-14 15:02:11 +0800

    modify Caroousel demo source

--- dev/components/carousel.js
+++ dev/components/carousel.js
@@ -10,16 +10,16 @@ class CarouselDev extends Component {
                 <div style={{height: 272,width: 704}}>
                     <Carousel autoPlay={false}>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/720d294969878186fa9b5777f3034408/%E5%95%86%E4%B8%9A%E6%8E%A8%E5%B9%BF%E5%A4%A7%E5%9B%BE.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144722jvsrm.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/afef9051299c1462283983fcd7d91319/8e37b061c69482bd6d8f3677c5102d72.png" />
+                            <img src="http://upload.ouliu.net/i/20200114144742ntcxr.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6f6220b2c3ff49ce405364b100719841/d938bab2cb1e9adbfa383f3938412fab.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144757x2un1.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6cc55c100ebae47a71011ef61a0c3034/61b74bfa90a76392d5dcd92b1f2dd432.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144813v02q8.jpeg" />
                         </Carousel.Item>
                     </Carousel>
                 </div>
@@ -29,16 +29,16 @@ class CarouselDev extends Component {
                 <div style={{height: 272,width: 704}}>
                     <Carousel arrowTrigger="never">
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/720d294969878186fa9b5777f3034408/%E5%95%86%E4%B8%9A%E6%8E%A8%E5%B9%BF%E5%A4%A7%E5%9B%BE.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144722jvsrm.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/afef9051299c1462283983fcd7d91319/8e37b061c69482bd6d8f3677c5102d72.png" />
+                            <img src="http://upload.ouliu.net/i/20200114144742ntcxr.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6f6220b2c3ff49ce405364b100719841/d938bab2cb1e9adbfa383f3938412fab.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144757x2un1.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6cc55c100ebae47a71011ef61a0c3034/61b74bfa90a76392d5dcd92b1f2dd432.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144813v02q8.jpeg" />
                         </Carousel.Item>
                     </Carousel>
                 </div>
@@ -51,16 +51,16 @@ class CarouselDev extends Component {
                         arrowTrigger="hover"
                         autoPlay={false}>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/720d294969878186fa9b5777f3034408/%E5%95%86%E4%B8%9A%E6%8E%A8%E5%B9%BF%E5%A4%A7%E5%9B%BE.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144722jvsrm.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/afef9051299c1462283983fcd7d91319/8e37b061c69482bd6d8f3677c5102d72.png" />
+                            <img src="http://upload.ouliu.net/i/20200114144742ntcxr.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6f6220b2c3ff49ce405364b100719841/d938bab2cb1e9adbfa383f3938412fab.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144757x2un1.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6cc55c100ebae47a71011ef61a0c3034/61b74bfa90a76392d5dcd92b1f2dd432.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144813v02q8.jpeg" />
                         </Carousel.Item>
                     </Carousel>
                 </div>
@@ -70,16 +70,16 @@ class CarouselDev extends Component {
                 <div style={{height: 272,width: 704}}>
                     <Carousel effect="fade">
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/720d294969878186fa9b5777f3034408/%E5%95%86%E4%B8%9A%E6%8E%A8%E5%B9%BF%E5%A4%A7%E5%9B%BE.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144722jvsrm.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/afef9051299c1462283983fcd7d91319/8e37b061c69482bd6d8f3677c5102d72.png" />
+                            <img src="http://upload.ouliu.net/i/20200114144742ntcxr.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6f6220b2c3ff49ce405364b100719841/d938bab2cb1e9adbfa383f3938412fab.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144757x2un1.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6cc55c100ebae47a71011ef61a0c3034/61b74bfa90a76392d5dcd92b1f2dd432.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144813v02q8.jpeg" />
                         </Carousel.Item>
                     </Carousel>
                 </div>
@@ -89,16 +89,16 @@ class CarouselDev extends Component {
                 <div style={{height: 272,width: 704}}>
                     <Carousel effect="smash" autoPlay={false}>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/720d294969878186fa9b5777f3034408/%E5%95%86%E4%B8%9A%E6%8E%A8%E5%B9%BF%E5%A4%A7%E5%9B%BE.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144722jvsrm.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/afef9051299c1462283983fcd7d91319/8e37b061c69482bd6d8f3677c5102d72.png" />
+                            <img src="http://upload.ouliu.net/i/20200114144742ntcxr.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6f6220b2c3ff49ce405364b100719841/d938bab2cb1e9adbfa383f3938412fab.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144757x2un1.jpeg" />
                         </Carousel.Item>
                         <Carousel.Item>
-                            <img src="https://edu-ad-test-cdn.cdn.bcebos.com/6cc55c100ebae47a71011ef61a0c3034/61b74bfa90a76392d5dcd92b1f2dd432.jpg" />
+                            <img src="http://upload.ouliu.net/i/20200114144813v02q8.jpeg" />
                         </Carousel.Item>
                     </Carousel>
                 </div>
